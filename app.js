0aaafcc297e35dabc68910dfd9c1292bd7ece14c
cFace <qwerty151199creepy@gmail.com>
2020-11-23 11:01:53 +0100

    code clean-up

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const logger = require("pino")();
 
 const PORT = process.env.PORT || 9601;
 const INDEX = "/index.html";
 
-const express = require("express");
+const logger = require("pino")();
 
-const server = express()
+const server = require("express")()
     .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-    .listen(PORT, () => logger.info("Server listening on port " + PORT));
+    .listen(PORT, () => l_listen(PORT));
 
 const { Server } = require("ws");
 const wss = new Server({ server });
-
 
 class gameBoard {
     board = [
@@ -52,7 +50,7 @@ class gameBoard {
         return false;
     };
     dropPiece(col, client) {
-        var i = 6;
+        var i = 5 + 1;
         do {
             i--;
             if (i < 0) return false;
@@ -62,127 +60,122 @@ class gameBoard {
         return true;
     };
 };
+class sessionClass {
+    constructor(ws, name, color, pass) {
+        this.clients = [];
+        this.clients.push({socket: ws, name: name, color: color, index: 0 });
+        this.gameBoard = new gameBoard();
+        this.passcode = pass;
+        this.properties = {};
+        this.isReady = false;
+        this.timer = configTimer(pass);
+    }
 
+    ready(ws, name, color) {
+        this.clients.push({ socket: ws, name: name, color: color, index: 1 });
+        this.properties.playerTurn = this.clients[Math.round(Math.random())];
+        this.isReady = true;
+    }
+    remRet(pass, map) {
+        var x = this;
+        clearTimeout(this.timer);
+        map.delete(pass);
+        return x;
+    }
+    flipTurn() {
+        if (this.properties.playerTurn.index == 0) {
+            this.properties.playerTurn = this.clients[1];
+        } else this.properties.playerTurn = this.clients[0];
+    }
+
+};
 var pendingMap = new Map();
 var ongoingMap = new Map();
 
 wss.on('connection', function connection(ws) {
-    logger.info("Received connection from " + ws._socket.remoteAddress);
+    l_recConn(ws._socket.remoteAddress);
     ws.on('message', function (message) {
         var data = JSON.parse(message);
-        var pass = data.passcode;
         switch (data.type) {
             case "session":
-                handleSession(pass, data, ws);
+                handleSession(data, ws);
                 break;
             case "choice":
-                handleChoice(pass, data, ws);
+                handleChoice(data, ws);
                 break;
         }
     });
     ws.on("close", function () {
         var result = isUserInMap(pendingMap, ws);
         if (result) {
-            logger.info("Session in pending list canceled(passcode: " + result.key + ", reason: userDisconnected)");
-            clearTimeout(pendingMap.get(result.key).timer);
-            pendingMap.delete(result.key);
+            l_cancPend(result.pass);
+            clearTimeout(pendingMap.get(result.pass).timer);
+            pendingMap.delete(result.pass);
         }
-        var result = isUserInMap(ongoingMap, ws);
+        result = isUserInMap(ongoingMap, ws);
         if (result) {
-            logger.info("Session surrendered(passcode: " + result.key + ", winner: " + result.client.name + ")");
-            endSession(result.key, "surrender",
-                ongoingMap.get(result.key).clients[((ongoingMap.get(result.key).clients.indexOf(result.client) == 0) ? 1 : 0)].name
+            l_surr(result.pass, result.client.name);
+            endSession(result.pass, "surrender",
+                ongoingMap.get(result.pass).clients[(result.client.index == 0) ? 1 : 0].name
             );
         }
     })
 });
 
-function handleSession(pass, data, ws) {
+function handleSession(data, ws) {
+    var pass = data.passcode;
     if (pendingMap.has(pass)) {
-        var session = pendingMap.get(pass);
-        clearTimeout(pendingMap.get(pass).timer);
-        pendingMap.delete(pass);
-        session.clients.push({ socket: ws, name: data.name, color: 1, colorName: "yellow" });
-        session.properties.playerTurn = session.clients[Math.round(Math.random())];
+        var session = pendingMap.get(pass).remRet(pass, pendingMap);
+        session.ready(ws, data.name, 2);
         ongoingMap.set(pass, session);
-        logger.info("Session moved from waiting list to active list (passcode: " + pass + ", player0 : "
-            + session.clients[0].name + ", player1 :" + session.clients[1].name + ")");
+        l_movePend(pass, session.clients[0].name, session.clients[1].name);
+        sendSessionDetails(session);
         updateTurnInfo(session);
     }
     else if (ongoingMap.has(pass)) {
-        logger.info("Session with in-use name was tried to be created (passcode: " + pass + ", player0: "
-            + data.name + ")");
-        ws.send(JSON.stringify({
-            type: "error",
-            error: "Passcode currently in-use, try another one!"
-        }));
+        l_inUseSess(pass, data.name);
+        ws.send(JSON.stringify({ type: "error", code: 5 }));
     } else {
-        var sessionInstance = {
-            clients: [{ socket: ws, name: data.name, color: 2, colorName: "red" }],
-            gameBoard: new gameBoard(),
-            passcode: pass,
-            properties: {},
-            timer: configTimer(pass)
-        };
-        pendingMap.set(pass, sessionInstance);
-        logger.info("Session added to the waiting list(passcode: " + pass + ", player0: "
-            + sessionInstance.clients[0].name + ")");
+        pendingMap.set(pass, new sessionClass(ws, data.name, 1, pass));
+        l_addPend(pass, pendingMap.get(pass).clients[0].name);
     }
 }
 
 function isUserInMap(map, ws) {
-    var x = false;
-    map.forEach((values, keys) => {
-        if (x === false) {
-            values.clients.forEach((item, index) => {
-                if (x === false) {
-                    if (ws == item.socket) {
-                        x = {
-                            map: map,
-                            key: keys,
-                            client: values.clients[index]
-                        };
-                    }
-                }
-            })
+    for (const session of map) {
+        if (session[1].clients !== undefined) {
+            for (const client of session[1].clients) {
+                if (ws == client.socket)
+                    return { pass: session[1].passcode, client: client };
+            }
         }
-    });
-    return x;
+    }
 }
 
-
-function handleChoice(pass, data, ws) {
+function handleChoice(data, ws) {
+    var pass = data.passcode;
+    if (!ongoingMap.has(pass) || !ongoingMap.get(pass).isReady) return;
     var session = ongoingMap.get(pass);
-    if (session.properties.playerTurn.socket !== ws) {
-        ws.send(JSON.stringify({
-            type: "error",
-            errorMsg: "It's not your turn!"
-        }));
+    var turn = session.properties.playerTurn;
+    if (turn.socket !== ws) {
+        ws.send(JSON.stringify({ type: "error", code: 6 }));
         return;
     }
-    if (session.gameBoard.dropPiece(data.value,
-        session.properties.playerTurn) === false) return;
-
-    wss.broadcast(JSON.stringify({
-        type: "BoardUpdate",
-        board: session.gameBoard.board
-    }), pass);
-
-    logger.info("Session game update (passcode: " + pass + ", player0: "
-        + session.clients[0].name + ", player1: " + session.clients[1].name + ")");
-    var check = session.gameBoard.checkWinConditions(session.properties.playerTurn.color);
+    if (session.gameBoard.dropPiece(data.value, turn) === false) return;
+    wss.broadcast(JSON.stringify({ type: "BoardUpdate", board: session.gameBoard.board }), pass);
+    l_actUpd(pass, session.clients[0].name, session.clients[1].name);
+    var check = session.gameBoard.checkWinConditions(turn.color);
     if (check !== false) {
-        logger.info("Session terminated (passcode: " + pass + ", winner: " + session.properties.playerTurn.name + ")");
-        endSession(pass, check, session.properties.playerTurn.name);
+        l_actOver(pass, turn.name);
+        endSession(pass, check, turn.name);
         return;
     }
     updateTurnInfo(session);
 }
 
-
 function configTimer(pass) {
     return setTimeout(function () {
-        logger.info("Session expired (passcode: " + pass + ")");
+        l_pendExp(pass);
         pendingMap.get(pass).clients[0].socket.send(JSON.stringify({
             type: "info",
             msg: "Session expired create another one!"
@@ -191,27 +184,33 @@ function configTimer(pass) {
     }, 60000);
 }
 
-function endSession(passcode, reason, winner) {
-    var response = {
-        type: "gameOver",
-        reason: reason,
-        winner: winner
+function endSession(pass, reason, winner) {
+    wss.broadcast(JSON.stringify({ type: "gameOver", reason: reason, winner: winner }), pass);
+    ongoingMap.delete(pass);
+}
+
+function sendSessionDetails(session) {
+    var info = {
+        type: "sessionConfirmation",
+        color: null,
+        name: null
+    };
+    for (var index = 0; index < 2; index++) {
+        info.color = session.clients[index].color;
+        info.name = session.clients[index].name;
+        session.clients[index].socket.send(JSON.stringify(info));
     }
-    wss.broadcast(JSON.stringify(response), passcode);
-    ongoingMap.delete(passcode);
 }
 
 function updateTurnInfo(session) {
-    session.properties.playerTurn = session.clients[(session.properties.playerTurn == session.clients[0]) ? 1 : 0];
+    session.flipTurn();
     var jsonInfo = {
         type: "info",
         code: "i_Turn",
-        color: null,
         turnColor: null,
         turnName: null
     };
     for (var index = 0; index < 2; index++) {
-        jsonInfo.color = session.clients[index].color
         jsonInfo.turnColor = session.properties.playerTurn.color;
         jsonInfo.turnName = session.properties.playerTurn.name;
         session.clients[index].socket.send(JSON.stringify(jsonInfo));
@@ -223,3 +222,18 @@ wss.broadcast = function broadcast(msg, passcode) {
         client.socket.send(msg);
     });
 };
+
+
+//logger prints
+function l_listen(port) { l_log("Server listening on port " + port) }
+function l_recConn(address) { l_log("Received connection from " + address) }
+function l_cancPend(pass) { l_log("Session in pending list canceled(passcode: " + pass + ", reason: userDisconnected)") }
+function l_surr(pass, name) { l_log("Session surrendered(passcode: " + pass + ", winner: " + name + ")") }
+function l_movePend(pass, p0, p1) { l_log("Session moved from waiting list to active list (passcode: " + pass + ", player0 : " + p0 + ", player1 :" + p1 + ")") }
+function l_inUseSess(pass, name) { l_log("Session with in-use name was tried to be created (passcode: " + pass + ", player0: " + name + ")") }
+function l_addPend(pass, name) { l_log("Session added to the waiting list(passcode: " + pass + ", player0: " + name + ")") }
+function l_actUpd(pass, p0, p1) { l_log("Session game update (passcode: " + pass + ", player0: " + p0 + ", player1: " + p1 + ")") }
+function l_actOver(pass, name) { l_log("Session terminated (passcode: " + pass + ", winner: " + name + ")") }
+function l_pendExp(pass) { l_log("Session expired (passcode: " + pass + ")") }
+
+function l_log(msg) { logger.info(msg); }
